dce451b2b35e5e0b77932af9cdf864f968ec8234
lowjunen <lowjunen@gmail.com>
2020-04-05 18:11:43 -0700

    Add working rtd base folder.

--- Docs/source/conf.py
+++ Docs/source/conf.py
@@ -2,7 +2,7 @@
 #
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
 
@@ -17,17 +17,22 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'msl quad'
-copyright = '2019, msl'
-author = 'msl'
+project = 'MSL Quad'
+copyright = '2020, MSL'
+author = 'MSL'
+
+# The full version, including alpha/beta/rc tags
+release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
+import sphinx_rtd_theme
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx_rtd_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -36,7 +41,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -44,7 +49,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
---
+++ Docs/source/index.rst
@@ -0,0 +1,33 @@
+.. MSL Quad documentation master file, created by
+   sphinx-quickstart on Sun Apr  5 16:04:10 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+############################
+MSL Quadcopter Documentation
+############################
+
+Introduction
+============
+Welcome to the MSL Quadcopter documentation page.
+Here we describe the steps necessary for setting 
+up its hardware and software for flight tests in
+the Stanford Flightroom.
+
+Hardware
+========
+
+.. toctree::
+   :maxdepth: 2
+   :titlesonly:
+
+   HardwareDocs/Installation
+
+Software
+========
+
+.. toctree::
+   :maxdepth: 2
+   :titlesonly:
+
+   SoftwareDocs/Installation
