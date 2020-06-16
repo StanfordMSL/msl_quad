77b9cabcc211f7f25c4ccb81acc2e01389930e0c
Alexander Koufos <exoticdft@gmail.com>
2020-06-15 18:21:45 -0700

    Adds the ability to handle Markdown files
    - Adds recommonmark as a sphinx extension so we can also write markdown files for documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,8 @@ import sphinx_rtd_theme
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx_rtd_theme"
+    "sphinx_rtd_theme",
+    "recommonmark"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
