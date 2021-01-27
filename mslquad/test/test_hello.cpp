edc9eb1d622922dce2fa8319b26d1d2cbb512eb9
Kunal Shah <kshah.kunal@gmail.com>
2021-01-26 17:34:16 -0800

    almost there

--- mslquad/test/test_hello.cpp
+++ mslquad/test/test_hello.cpp
@@ -6,19 +6,26 @@
 // Declare a test
 TEST(TestSuite, testHello){
   EXPECT_EQ("hello", "hello");
-  }
+}
 
 // Declare another test
 TEST(TestSuite, testSucceed){
   SUCCEED();
-  }
+}
 
 TEST(TestSuite, testAssert){
   int n = 1;
   ASSERT_EQ(n, 1);
-  }
+}
+
+TEST(TestSuite, testFloat){
+  float a,b;
+  a = 1;
+  b = .5;
+  ASSERT_EQ(a+b, 1.5);
+}
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv){
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
-  }
+}
