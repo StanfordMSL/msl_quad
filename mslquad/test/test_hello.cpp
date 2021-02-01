788489ba0cb22e89cf5964796fef9ad08365bb7d
Kunal Shah <kshah.kunal@gmail.com>
2021-02-01 13:22:04 -0800

    lint

--- mslquad/test/test_hello.cpp
+++ mslquad/test/test_hello.cpp
@@ -1,31 +1,33 @@
+// copyright[2019] <msl>
 // Bring in package's API
 #include <mslquad/px4_base_controller.h>
  // Bring in gtest
 #include <gtest/gtest.h>
- 
+
 // Declare a test
-TEST(TestSuite, testHello){
+TEST(TestSuite, testHello) {
   EXPECT_EQ("hello", "hello");
 }
 
 // Declare another test
-TEST(TestSuite, testSucceed){
+TEST(TestSuite, testSucceed) {
   SUCCEED();
 }
 
-TEST(TestSuite, testAssert){
+TEST(TestSuite, testAssert) {
   int n = 1;
   ASSERT_EQ(n, 1);
 }
 
-TEST(TestSuite, testFloat){
-  float a,b;
+TEST(TestSuite, testFloat) {
+  float a, b;
   a = 1;
   b = .5;
   ASSERT_EQ(a+b, 1.5);
 }
+`
 // Run all the tests that were declared with TEST()
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
